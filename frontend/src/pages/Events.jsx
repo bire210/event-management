43a0da6245f457dd24105145045d429b2973bdab
birendrafuration <birendra.mahto@furation.tech>
2023-11-23 17:58:57 +0530

    added event

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { ContextState } from "../context/Context";
 
 const Events = () => {
+  const {setUser}=ContextState()
+  const user=JSON.parse(localStorage.getItem("userInfo"));
+  setUser(user);
+  console.log(user)
   return (
     <div className="flex text-center h-screen justify-center items-center bg-slate-400">
       Events
